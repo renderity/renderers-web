d053bac139ec25376216c56cdd3b425f8bf7cc87
dsbelov63 <dsbelov63@gmail.com>
2022-02-06 08:14:29 +0400

    removed wasm wrapper code

---
+++ src/index.js
@@ -0,0 +1,373 @@
+import getWebgl from './webgl';
+import getWebgpu from './webgpu';
+
+
+
+export default class Renderers
+{
+	constructor (wasm_wrapper)
+	{
+		const WasmWrapper = wasm_wrapper.constructor;
+
+
+
+		class Base
+		{
+			static instances = null;
+
+			static getInstance (addr, ...args)
+			{
+				if (!this.instances)
+				{
+					this.instances = {};
+				}
+
+				if (!this.instances[addr])
+				{
+					Object.defineProperty
+					(
+						this.instances,
+
+						addr,
+
+						{ value: new this(addr, ...args) },
+					);
+				}
+
+				return this.instances[addr];
+			}
+
+			static getOriginalStruct (addr)
+			{
+				const original_struct = {};
+
+				let member_index = 0;
+
+				for (const member_name in this.original_struct_descriptor)
+				{
+					const type = this.original_struct_descriptor[member_name];
+
+					original_struct[member_name] =
+						wasm_wrapper[type](addr + this.original_struct_offsets[member_index]);
+
+					++member_index;
+				}
+
+				return original_struct;
+			}
+
+
+
+			constructor (addr)
+			{
+				this.addr = addr;
+			}
+		}
+
+
+
+		class RendererBase extends Base
+		{
+			static original_struct_descriptor =
+				{
+					width: 'SizeT',
+					height: 'SizeT',
+				};
+
+			static original_struct_offsets =
+				wasm_wrapper.SizeTv
+				(
+					wasm_wrapper.exports_demangled['RDTY::WRAPPERS::renderer_offsets'],
+					Object.keys(this.original_struct_descriptor).length,
+				);
+
+
+
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.original_struct = RendererBase.getOriginalStruct(this.addr);
+
+				this.width = this.original_struct.width;
+				this.height = this.original_struct.height;
+			}
+		}
+
+		this.RendererBase = RendererBase;
+
+
+
+		class UniformBase extends Base
+		{
+			static original_struct_descriptor =
+				{
+					object_addr: 'Addr',
+					name: 'StdString',
+					// TODO: rename to offset
+					block_index: 'SizeT',
+					size: 'SizeT',
+				};
+
+			static original_struct_offsets =
+				wasm_wrapper.SizeTv
+				(
+					wasm_wrapper.exports_demangled['RDTY::WRAPPERS::uniform_offsets'],
+					Object.keys(this.original_struct_descriptor).length,
+				);
+
+
+
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.original_struct = UniformBase.getOriginalStruct(this.addr);
+
+				this.object_addr = this.original_struct.object_addr;
+
+				this.name = WasmWrapper.uint8Array2DomString(this.original_struct.name);
+
+				// uniform block index
+				this.block_index = this.original_struct.block_index;
+
+				this.size = this.original_struct.size;
+
+				this._data = wasm_wrapper.Charv2(this.object_addr, this.size);
+			}
+		}
+
+		this.UniformBase = UniformBase;
+
+
+
+		class UniformBlockBase extends Base
+		{
+			static original_struct_descriptor =
+				{
+					binding: 'SizeT',
+					type: 'SizeT',
+					name: 'StdString',
+					uniforms: 'StdVectorAddr',
+				};
+
+			static original_struct_offsets =
+				wasm_wrapper.SizeTv
+				(
+					wasm_wrapper.exports_demangled['RDTY::WRAPPERS::uniform_block_offsets'],
+					Object.keys(this.original_struct_descriptor).length,
+				);
+
+
+
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.original_struct = UniformBlockBase.getOriginalStruct(this.addr);
+
+				this.binding = this.original_struct.binding;
+
+				this.type = this.original_struct.type;
+
+				this.name = WasmWrapper.uint8Array2DomString(this.original_struct.name);
+
+
+
+				this.uniforms_seq = null;
+				this.uniforms_dict = {};
+
+				this.buffer = null;
+
+				this.buffer_length = 0;
+			}
+
+			getUniforms (renderer)
+			{
+				this.uniforms_seq =
+					// TypedArray::map returns TypedArray, but need Array.
+					Array.from(this.original_struct.uniforms).map
+					(
+						(uniform_addr) =>
+						{
+							const uniform = renderer.Uniform.getInstance(uniform_addr);
+
+							this.buffer_length += uniform._data.length;
+
+							this.uniforms_dict[uniform.name] = uniform;
+
+							return uniform;
+						},
+					);
+			}
+		}
+
+		this.UniformBlockBase = UniformBlockBase;
+
+
+
+		class DescriptorSetBase extends Base
+		{
+			static original_struct_descriptor =
+				{
+					bindings: 'StdVectorAddr',
+				};
+
+			static original_struct_offsets =
+				wasm_wrapper.SizeTv
+				(
+					wasm_wrapper.exports_demangled['RDTY::WRAPPERS::descriptor_set_offsets'],
+					Object.keys(this.original_struct_descriptor).length,
+				);
+
+
+
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.original_struct = DescriptorSetBase.getOriginalStruct(this.addr);
+
+				// this.uniform_blocks
+			}
+		}
+
+		this.DescriptorSetBase = DescriptorSetBase;
+
+
+
+		class MaterialBase extends Base
+		{
+			static original_struct_descriptor =
+				{
+					topology: 'SizeT',
+					front_face: 'SizeT',
+					blend_enabled: 'SizeT',
+					blend_color_op: 'SizeT',
+					blend_color_factor_src: 'SizeT',
+					blend_color_factor_dst: 'SizeT',
+					blend_alpha_op: 'SizeT',
+					blend_alpha_factor_src: 'SizeT',
+					blend_alpha_factor_dst: 'SizeT',
+					glsl100es_code_vertex: 'StdString',
+					glsl100es_code_fragment: 'StdString',
+					glsl300es_code_vertex: 'StdString',
+					glsl300es_code_fragment: 'StdString',
+					glsl4_code_vertex: 'StdString',
+					glsl4_code_fragment: 'StdString',
+					glsl_vulkan_code_vertex: 'StdString',
+					glsl_vulkan_code_fragment: 'StdString',
+					wgsl_code_vertex: 'StdString',
+					wgsl_code_fragment: 'StdString',
+					spirv_code_vertex: 'StdVectorUint32',
+					spirv_code_fragment: 'StdVectorUint32',
+					uniforms: 'StdVectorAddr',
+					uniform_blocks: 'StdVectorAddr',
+					descriptor_sets: 'StdVectorAddr',
+				};
+
+			static original_struct_offsets =
+				wasm_wrapper.SizeTv
+				(
+					wasm_wrapper.exports_demangled['RDTY::WRAPPERS::material_offsets'],
+					Object.keys(this.original_struct_descriptor).length,
+				);
+
+			static used_instance = null;
+
+
+
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.original_struct = MaterialBase.getOriginalStruct(this.addr);
+
+
+
+				this.topology = this.constructor.TOPOLOGY[this.original_struct.topology];
+
+				this.front_face = this.constructor.FRONT_FACE[this.original_struct.front_face];
+
+				this.blend_enabled = this.constructor.BLEND_ENABLED[this.original_struct.blend_enabled];
+
+				this.blend_color_op =
+					this.constructor.BLEND_OP[this.original_struct.blend_color_op];
+
+				this.blend_color_factor_src =
+					this.constructor.BLEND_FACTOR[this.original_struct.blend_color_factor_src];
+
+				this.blend_color_factor_dst =
+					this.constructor.BLEND_FACTOR[this.original_struct.blend_color_factor_dst];
+
+				this.blend_alpha_op =
+					this.constructor.BLEND_OP[this.original_struct.blend_alpha_op];
+
+				this.blend_alpha_factor_src =
+					this.constructor.BLEND_FACTOR[this.original_struct.blend_alpha_factor_src];
+
+				this.blend_alpha_factor_dst =
+					this.constructor.BLEND_FACTOR[this.original_struct.blend_alpha_factor_dst];
+
+				// this.uniforms
+				// this.uniform_blocks
+			}
+		}
+
+		this.MaterialBase = MaterialBase;
+
+
+
+		class ObjectBase extends Base
+		{
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.scene_vertex_data_offset = wasm_wrapper.SizeT(addr + (WasmWrapper.PTR_SIZE * 2));
+				this.scene_vertex_data_length = wasm_wrapper.SizeT(addr + (WasmWrapper.PTR_SIZE * 3));
+				this.vertex_data = wasm_wrapper.StdVectorFloat(addr + (WasmWrapper.PTR_SIZE * 4));
+			}
+		}
+
+		this.ObjectBase = ObjectBase;
+
+
+
+		class SceneBase extends Base
+		{
+			constructor (addr)
+			{
+				super(addr);
+
+
+
+				this.vertex_data = wasm_wrapper.StdVectorFloat(addr + (WasmWrapper.PTR_SIZE * 2));
+			}
+
+			addObject (object_addr)
+			{
+				wasm_wrapper.exports_demangled['RDTY::WRAPPERS::Scene::addObject()'](this.addr, object_addr);
+			}
+		}
+
+		this.SceneBase = SceneBase;
+
+
+
+		this.WebGL = getWebgl(this);
+		this.WebGPU = getWebgpu(this);
+	}
+}
---
+++ src/webgl.js
@@ -0,0 +1,465 @@
+/*
+eslint-disable
+
+no-undefined,
+*/
+
+
+
+const getWebgl =
+({
+	wasm_wrapper,
+	RendererBase,
+	UniformBase,
+	UniformBlockBase,
+	MaterialBase,
+	ObjectBase,
+	SceneBase,
+}) =>
+{
+	class WebGL
+	{
+		constructor ()
+		{
+			const WasmWrapper = wasm_wrapper.constructor;
+
+
+
+			class Renderer extends RendererBase
+			{
+				// dpr
+				// renderer info (gl.getParameter(gl.SHADING_LANGUAGE_VERSION)...)
+				constructor (addr_renderer, canvas, _context = 'webgl')
+				{
+					super(addr_renderer);
+
+					this.exists = true;
+
+
+
+					/* eslint-disable-next-line consistent-this */
+					const renderer = this;
+
+
+
+					this.canvas = canvas || document.createElement('canvas');
+
+					this.canvas.width = this.original_struct.width;
+					this.canvas.height = this.original_struct.height;
+
+					this.canvas.style.width = `${ this.original_struct.width }px`;
+					this.canvas.style.height = `${ this.original_struct.height }px`;
+
+					this._context = this.canvas.getContext(_context);
+
+					if (!this._context)
+					{
+						this.exists = false;
+
+						return undefined;
+					}
+
+
+
+					this.gpu_resources = [];
+
+
+
+					this.loop_function = null;
+					this.loop_function_wrapper = null;
+
+
+
+					const gl = this._context;
+
+					gl.viewport(0, 0, this.original_struct.width, this.original_struct.height);
+
+
+
+					class Uniform extends UniformBase
+					{
+						constructor (addr)
+						{
+							super(addr);
+
+
+
+							this.location = null;
+
+							// TODO: add multiple typed data initializers.
+							this.typed_data = wasm_wrapper.Floatv(this.object_addr, this.size / 4);
+						}
+					}
+
+					this.Uniform = Uniform;
+
+
+
+					class UniformBlock extends UniformBlockBase
+					{
+						constructor (addr)
+						{
+							super(addr);
+
+
+
+							this.getUniforms(renderer);
+
+							this.buffer = gl.createBuffer();
+
+							renderer.gpu_resources.push([ 'deleteBuffer', this.buffer ]);
+
+							gl.bindBuffer(gl.UNIFORM_BUFFER, this.buffer);
+							gl.bindBufferBase(gl.UNIFORM_BUFFER, this.binding, this.buffer);
+							gl.bufferData(gl.UNIFORM_BUFFER, this.buffer_length, gl.DYNAMIC_DRAW);
+
+							// Initially update uniforms.
+							this.use();
+
+							// gl.bindBuffer(gl.UNIFORM_BUFFER, null);
+						}
+
+						use ()
+						{
+							gl.bindBuffer(gl.UNIFORM_BUFFER, this.buffer);
+
+							for
+							(
+								let uniform_index = 0;
+								uniform_index < this.uniforms_seq.length;
+								++uniform_index
+							)
+							{
+								const uniform = this.uniforms_seq[uniform_index];
+
+								gl.bufferSubData(gl.UNIFORM_BUFFER, uniform.block_index, uniform._data);
+							}
+						}
+					}
+
+					this.UniformBlock = UniformBlock;
+
+
+
+					class Material extends MaterialBase
+					{
+						static TOPOLOGY =
+							[
+								gl.TRIANGLES,
+								gl.POINTS,
+								gl.LINES,
+							];
+
+						static FRONT_FACE =
+							[
+								gl.CCW,
+								gl.CW,
+							];
+
+						static BLEND_ENABLED =
+							[
+								false,
+								true,
+							];
+
+						static BLEND_FACTOR =
+							[
+								gl.ZERO,
+								gl.ONE,
+							];
+
+						static BLEND_OP =
+							[
+								gl.FUNC_ADD,
+								gl.FUNC_SUBTRACT,
+								gl.FUNC_REVERSE_SUBTRACT,
+								gl.MIN || this.extensions?.EXT_blend_minmax?.MIN_EXT || null,
+								gl.MAX || this.extensions?.EXT_blend_minmax?.MAX_EXT || null,
+							];
+
+
+
+						constructor (addr)
+						{
+							super(addr);
+
+
+
+							if
+							(
+								this.original_struct.blend_color_op === null ||
+								this.original_struct.blend_alpha_op === null
+							)
+							{
+								// LOG('Enable "EXT_blend_minmax" extension!');
+							}
+
+
+
+							this.uniforms_seq = null;
+							this.uniforms_dict = {};
+
+
+
+							this.program = gl.createProgram();
+
+							renderer.gpu_resources.push([ 'deleteProgram', this.program ]);
+
+
+
+							// vertex
+							{
+								let code = null;
+
+								if (renderer._context.constructor === WebGLRenderingContext)
+								{
+									code =
+									WasmWrapper.uint8Array2DomString(this.original_struct.glsl100es_code_vertex).trim();
+								}
+								else if (renderer._context.constructor === WebGL2RenderingContext)
+								{
+									code =
+									WasmWrapper.uint8Array2DomString(this.original_struct.glsl300es_code_vertex).trim();
+								}
+
+								const shader = gl.createShader(gl.VERTEX_SHADER);
+
+								renderer.gpu_resources.push([ 'deleteShader', shader ]);
+
+								gl.shaderSource(shader, code);
+
+								gl.compileShader(shader);
+
+								if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+								{
+									const strOut =
+									`\n${ code.split('\n').map((elm, i) => `${ i + 1 }:${ elm }`).join('\n') }\n`;
+
+									throw new Error(`${ strOut }${ gl.getShaderInfoLog(shader) }`);
+								}
+
+								gl.attachShader(this.program, shader);
+							}
+
+
+
+							// fragment
+							{
+								let code = null;
+
+								if (renderer._context.constructor === WebGLRenderingContext)
+								{
+									code =
+									WasmWrapper.uint8Array2DomString(this.original_struct.glsl100es_code_fragment).trim();
+								}
+								else if (renderer._context.constructor === WebGL2RenderingContext)
+								{
+									code =
+									WasmWrapper.uint8Array2DomString(this.original_struct.glsl300es_code_fragment).trim();
+								}
+
+								const shader = gl.createShader(gl.FRAGMENT_SHADER);
+
+								renderer.gpu_resources.push([ 'deleteShader', shader ]);
+
+								gl.shaderSource(shader, code);
+
+								gl.compileShader(shader);
+
+								if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+								{
+									const strOut =
+									`\n${ code.split('\n').map((elm, i) => `${ i + 1 }:${ elm }`).join('\n') }\n`;
+
+									throw new Error(`${ strOut }${ gl.getShaderInfoLog(shader) }`);
+								}
+
+								gl.attachShader(this.program, shader);
+							}
+
+
+
+							gl.linkProgram(this.program);
+
+
+
+							// gl.useProgram(this.program);
+
+							this.uniforms_seq =
+								// TypedArray::map returns TypedArray, but need Array.
+								Array.from(this.original_struct.uniforms)
+									.map
+									(
+										(uniform_addr) =>
+										{
+											const uniform = Uniform.getInstance(uniform_addr);
+
+											uniform.location = gl.getUniformLocation(this.program, uniform.name);
+
+											// Check if shader uses uniform then push uniform to this.uniforms.
+											if (uniform.location)
+											{
+												uniform.update = () =>
+												{
+													gl.uniformMatrix4fv(uniform.location, false, uniform.typed_data);
+												};
+
+												// uniform.update();
+
+												this.uniforms_dict[uniform.name] = uniform;
+
+												return uniform;
+											}
+
+											return null;
+										},
+									)
+									.filter((uniform) => uniform);
+
+							// gl.useProgram(null);
+
+
+
+							if (renderer._context.constructor === WebGL2RenderingContext)
+							{
+								this.original_struct.uniform_blocks.forEach
+								(
+									(uniform_block_addr) =>
+									{
+										const uniform_block = UniformBlock.getInstance(uniform_block_addr);
+
+										gl.uniformBlockBinding
+										(
+											this.program,
+											gl.getUniformBlockIndex(this.program, uniform_block.name),
+											uniform_block.binding,
+										);
+									},
+								);
+							}
+
+
+
+							// Initially update uniforms.
+							this.use();
+						}
+
+						use ()
+						{
+							Material.used_instance = this;
+
+							gl.useProgram(this.program);
+
+							gl.frontFace(this.front_face);
+
+							if (this.blend_enabled)
+							{
+								gl.enable(gl.BLEND);
+								gl.blendEquationSeparate(this.blend_color_op, this.blend_alpha_op);
+
+								gl.blendFuncSeparate
+								(
+									this.blend_color_factor_src,
+									this.blend_color_factor_dst,
+									this.blend_alpha_factor_src,
+									this.blend_alpha_factor_dst,
+								);
+							}
+							else
+							{
+								gl.disable(gl.BLEND);
+							}
+
+							this.uniforms_seq.forEach((uniform) => uniform.update());
+						}
+					}
+
+					this.Material = Material;
+
+
+
+					class _Object extends ObjectBase
+					{
+						draw ()
+						{
+							gl.drawArrays
+							(Material.used_instance.topology, this.scene_vertex_data_offset, this.scene_vertex_data_length);
+						}
+					}
+
+					this.Object = _Object;
+
+
+
+					// class ObjectIndexed extends Base
+					// {
+					// 	constructor (addr)
+					// 	{
+					// 		super(addr);
+					// 	}
+
+					// 	draw (renderer)
+					// 	{
+					// 		renderer.context.drawElements(this.topology);
+					// 	}
+					// };
+
+					// this.ObjectIndexed = ObjectIndexed;
+
+
+
+					class Scene extends SceneBase
+					{}
+
+					this.Scene = Scene;
+				}
+
+				// Generic methods for all renderers. TODO: share by using base class.
+				startLoop ()
+				{
+					this.loop_function_wrapper = () =>
+					{
+						this.loop_function();
+
+						this.animation_frame = requestAnimationFrame(this.loop_function_wrapper);
+					};
+
+					this.animation_frame = requestAnimationFrame(this.loop_function_wrapper);
+				}
+
+				endLoop ()
+				{
+					cancelAnimationFrame(this.animation_frame);
+				}
+
+				destroy ()
+				{
+					this.loop_function = null;
+					this.loop_function_wrapper = null;
+
+					this.gpu_resources
+						.reverse()
+						.forEach
+						(
+							(resource) =>
+							{
+								const [ delete_function_name, handle ] = resource;
+
+								this._context[delete_function_name](handle);
+							},
+						);
+
+					this.Uniform.instances = null;
+					this.UniformBlock.instances = null;
+					this.Material.instances = null;
+				}
+			}
+
+			this.Renderer = Renderer;
+		}
+	}
+
+	return WebGL;
+};
+
+export default getWebgl;
